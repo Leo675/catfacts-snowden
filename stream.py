bfe067638f9176cf17925ad61f5eb5a3a80675c5
Daytona <IDSninja@users.noreply.github.com>
2016-01-22 18:45:14 -0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,5 +1,5 @@
 #/usr/bin/python
-
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
@@ -12,8 +12,13 @@ import shutil
 config = SafeConfigParser()
 config.read('config.ini')
 
+#number file names
+main_numbers='numbers.txt'
+bad_meowers='bad_meow.txt'
+tmp_numbers='tmp_numbers.txt'
+
 #Unsubscribe string, case sensitive
-unsub_str='@Snowden Meow, I <3 catfacts'
+unsub_str='Meow, I &lt;3 catfacts'
 
 #Twitter API credentials: Fill them into config.ini, or replace the config.gets with yours.
 access_token = config.get('api_credentials', 'access_token')
@@ -22,31 +27,51 @@ consumer_key = config.get('api_credentials', 'consumer_key')
 consumer_secret = config.get('api_credentials', 'consumer_secret')
 
 #creates the numbers file if it doesnt exist.
-open('numbers.txt', 'a').close
-open('user_log.txt', 'a').close
+open(main_numbers, 'a').close
+open(bad_meowers, 'a').close
+open(tmp_numbers, 'a').close
 
+def unsubscribe(file_name, unsub_id):
+    with open(file_name,"r") as input:
+        with open(tmp_numbers,"wb") as output: 
+            for line in input:
+                if ':'+unsub_id not in line:
+                    output.write(line)
+    shutil.move(tmp_numbers, file_name)
+    open(tmp_numbers, 'a').close
+    
+    
 class StdOutListener(StreamListener):
+        
     def on_data(self, data):
         if '"text":' in data:
             tweet = json.loads(data)
             regex = '[^0][\s\(\-.]?([2-9]{1}[0-9]{2})[\s\)\-.]?[\s\(\-.]?([0-9]{3})[\s\)\-.]?[\s\(\-.]?([0-9]{4})[^0-9]'
             number = re.search(regex, tweet['text'])
+            if 'Meow' in tweet['text']:
+                print("\n"+tweet['text']+"\n")
             if number is not None:
                 raw_number = number.group(1)+number.group(2)+number.group(3)
-                fh = open('numbers.txt', 'r+a')
+                fh = open(main_numbers, 'r+a')
                 if raw_number not in fh.read():
                     print(tweet['text'].encode('utf-8'))
                     print(raw_number)
                     print(raw_number+':'+tweet['user']['id_str'], file=fh)
             #This is the unsub condition, feel free to remove the entire elif condition to disable it
-            elif unsub_str in tweet['text']:
-                with open("numbers.txt","r") as input:
-                    with open("tmp_numbers.txt","wb") as output: 
-                        for line in input:
-                            if tweet['user']['id_str'] not in line:
-                                output.write(line)
-                shutil.move('tmp_numbers.txt', 'numbers.txt')
-            
+            elif unsub_str in tweet['text'].encode('utf-8'):
+                unsubscribe(main_numbers, tweet['user']['id_str'])
+                print('REMOVING NUMBER '+tweet['user']['id_str']+' from '+main_numbers)
+                unsubscribe(bad_meowers, tweet['user']['id_str'])
+                print('REMOVING NUMBER '+tweet['user']['id_str']+' from '+bad_meowers)
+                
+            elif '@snowden' in tweet['text'].lower():
+                fh = open(bad_meowers, 'r+a')
+                with open(main_numbers,"r") as input:
+                    for line in input:
+                        if ':'+tweet['user']['id_str'] in line:
+                            if line not in fh.read():
+                                print(line, file=fh, end="")              
+
     def on_error(self, status):
         print(status)
 
@@ -56,4 +81,4 @@ if __name__ == '__main__':
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
-    stream.filter(track=['my number is', 'her number is', 'his number is', 'call me', 'text me', '@snowden Meow, I <3 catfacts'])
+    stream.filter(track=['my number is', 'her number is', 'his number is', 'call', 'text', '@snowden Meow'])
